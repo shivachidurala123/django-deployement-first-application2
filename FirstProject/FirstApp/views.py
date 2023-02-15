9c366d6007adb2707c5dba08ef62fcea2deed343
shivachidurala123 <125386145+shivachidurala123@users.noreply.github.com>
2023-02-15 18:09:10 +0530

    Update views.py
    updated FirstProject

--- FirstProject/FirstApp/views.py
+++ FirstProject/FirstApp/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def sample(request):
     ss='''
-        <h2>This is the sample view</h2>
+        <h2>Hello user!!</h2>
+        <hr/>
+        <h2>Welcome to django</h2>
+        <hr/>
+        <h2>All the best</h2>
         <hr/>
     
     '''
